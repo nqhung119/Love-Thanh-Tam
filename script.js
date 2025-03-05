a1491e537235f50b0e576e8efebddb17425672c9
Nguyen Quang Hung <121934030+nqhung119@users.noreply.github.com>
2025-03-06 00:47:02 +0700

    update

---
+++ script.js
@@ -0,0 +1,24 @@
+function updateTimeCounter() {
+    const startDate = new Date("2024-11-25T00:00:00");
+    const now = new Date();
+    const diff = now - startDate;
+
+    const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+    const months = Math.floor((diff % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30));
+    const days = Math.floor((diff % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
+    const totalDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    document.getElementById("years").textContent = years;
+    document.getElementById("months").textContent = months;
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+    document.getElementById("days-of-love").textContent = totalDays + " ngày";
+  }
+
+  updateTimeCounter();
+  setInterval(updateTimeCounter, 1000);
